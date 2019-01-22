dd5256e1cdc1d261c9a0bfe45e5de545661b6ce8
Jon Lee <jdukelee@gmail.com>
2019-01-22 14:13:57 -0800

    Update test form

--- egghead/structs-algos/test/queue.js
+++ egghead/structs-algos/test/queue.js
@@ -3,14 +3,8 @@ const {createQueue} = require('../queue');
 
 let q = createQueue();
 describe('Queue', function() {
-  describe('enqueue', function() {
-    it('should return 1', function() {
-      assert.equal(q.enqueue('First'), 1);
-    });
-
-    it('should return 2', function() {
-      assert.equal(q.enqueue('Second'), 2);
-    });
-
+  it('enqueue', function() {
+    assert.equal(q.enqueue('First'), 1);
+    assert.equal(q.enqueue('Second'), 2);
   });
 });

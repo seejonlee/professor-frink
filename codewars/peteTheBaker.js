fc3f813fc2c72c6ab98f499673dd1d13d1c3bedd
Jon Lee <jdukelee@gmail.com>
2019-02-25 06:17:25 -0800

    Add solution from codewars user

--- codewars/peteTheBaker.js
+++ codewars/peteTheBaker.js
@@ -29,5 +29,14 @@ function cakes(recipe, ingredients) {
   return (numOfCakes > 0 ? numOfCakes : 0);
 }
 
-console.log(cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}));
-console.log(cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000}));
+/**
+ * Top solution from Codewars
+ */
+function cakes2(recipe, available) {
+  return Object.keys(recipe).reduce(function(val, ingredient) {
+    return Math.min(Math.floor(available[ingredient] / recipe[ingredient] || 0), val)
+  }, Infinity)  
+}
+
+console.log(cakes2({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}));
+console.log(cakes2({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000}));

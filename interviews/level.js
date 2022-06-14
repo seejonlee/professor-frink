7e803833fdef58c1586dbc03909db3315f7b7158
Jon Lee <jdukelee@gmail.com>
2022-06-13 17:50:55 -0700

    Update: solution for compression scheme problem

--- interviews/level.js
+++ interviews/level.js
@@ -3,66 +3,54 @@ encode();
 decode();
 
 Examples:
-'aaabbcc' -> '3#52#b2#c'
+'aaabbccd' -> '3#52#b2#c'
 '###' -> '3##'
 'aaaaa' -> '5#a'
  */
 
-// If I don't get it, send to Rachel to please send to Eric.
-// Since i already didn't get the job there'd be no motivation for me to cheat a solution.
-// I did have to look up certain things, specifically the correct regex used in decode().
-
 function encode(str) {
-	// Loop the string
-	// For each character, see if the subsequent characters are the same
-	// While they are the same, continue looping the str
-	// Once they are different, append the encode symbol to the result.
+	let count = 1;
+	let prevChar = str.charAt(0);
 	let encodedStr = '';
 
-	for (let i = 0; i < str.length; i++) {
+	// Count the sequential runs of characters
+	for (let i = 1; i < str.length; i++) {
 		const currentChar = str.charAt(i);
-		let occurrence = 1;
 
-		for (let j = i + 1; j < str.length; j++) {
-			if (currentChar === str.charAt(j)) {
-				occurrence++;
-				i = j;
-			} else {
-				i = j - 1;
-				break;
-			}
+		if (currentChar === prevChar) {
+			count++;
+		} else {
+			// When reaching the end of a sequential run, replace the run with the encoded symbol
+			encodedStr = encodedStr.concat(`${count}#${prevChar}`);
+			prevChar = currentChar;
+			count = 1;
 		}
-
-		encodedStr += `${occurrence}#${currentChar}`;
 	}
+
+	encodedStr = encodedStr.concat(`${count}#${prevChar}`);
 
 	return encodedStr;
 }
 
 function decode(str) {
-	// Read each encoding symbol from 'str'
-	// For each symbol, write out the decoded sequence of characters.
-
-	// Use regular expression to extract the symbols.
-	const symbols = str.match(/(\d+#\w)/g);
 	let decodedStr = '';
+	const symbols = str.match(/([\d]+#[\w])/gm);
 
-	// Loop the symbols and write the decoded str
-	symbols?.forEach(symbol => {
-		const [occurence, value] = symbol.split('#');
-		const subStr = Array.from(new Array(Number.parseInt(occurence) - 1)).reduce((prev, current) => {
-			return prev.concat(value);
-		}, value);
+	symbols.forEach(sym => {
+		const values = sym.split('#');
+		const letters = Array(Number.parseInt(values[0])).fill(values[1]).reduce((acc, letter) => {
+			return acc.concat(letter);
+		});
 
-		decodedStr = decodedStr.concat(subStr);
+		decodedStr = decodedStr.concat(letters);
 	});
 
 	return decodedStr;
 }
 
 console.group('Set 1');
-console.log(encode('aaabbbccc2'));
-console.log(decode(encode('aaabbbccc2')));
+console.log(encode('aaaaaaaaaaaaabbbccc2'));
+console.log(decode(encode('aaaaaaaaaaaaabbbccc2')));
 console.groupEnd();
 
 console.group('Set 2');
